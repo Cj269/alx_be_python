3b9785d8f5d556695ffa51f2e103942dd8e3e5da
Caren Jerotich <carenjerotich26@gmail.com>
2025-06-10 21:39:37 +0300

    Add basic_operations

---
+++ basic_operations.py
@@ -0,0 +1,31 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+print ("Addition of", number1, "and", number2, "is", result)
+result = number1 - number2
+print ("Subtractuion of", number1, "and", number2, "is", result)
+result = number1 * number2
+print ("Multiplication of", number1, "and", number2, "is", result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
